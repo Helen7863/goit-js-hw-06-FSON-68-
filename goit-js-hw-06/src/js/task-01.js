7d151a802dacf57cedbfc1d49350152253fd93cc
Helen Biliak <lbelka7863@gmail.com>
2023-02-12 16:29:41 +0200

    1 task

---
+++ goit-js-hw-06/src/js/task-01.js
@@ -0,0 +1,7 @@
+const listRef = document.querySelectorAll('.item');
+console.log(`Number of categories: ${listRef.length}`)
+listRef.forEach(element => {
+    console.log('Category: ', element.querySelector('h2').textContent )
+    console.log('Elements: ', element.querySelectorAll('li').length)
+    
+});
